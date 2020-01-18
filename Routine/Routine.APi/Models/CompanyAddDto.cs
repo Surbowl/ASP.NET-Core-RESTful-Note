fd496fafc2e9b6664cbd9b2b6a22b8695060f985
碗碗 <43742453+Surbowl@users.noreply.github.com>
2020-01-18 20:31:22 +0800

    Update to p26

--- Routine/Routine.APi/Models/CompanyAddDto.cs
+++ Routine/Routine.APi/Models/CompanyAddDto.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Routine.APi.Models
 {
@@ -9,7 +7,15 @@ namespace Routine.APi.Models
     //查询、插入、更新应该使用不同的Dto，便于业务升级与重构
     public class CompanyAddDto
     {
+        [Display(Name = "公司名")]
+        [Required(ErrorMessage = "{0}这个字段是必填的")]
+        [MaxLength(100, ErrorMessage = "{0}的最大长度不可以超过{1}")]
         public string Name { get; set; }
+
+        [Display(Name = "简介")]
+        [StringLength(500,MinimumLength =10,ErrorMessage = "{0}的长度范围从{2}到{1}")]
+        //[MaxLength(500, ErrorMessage = "{0}的最大长度不可以超过{1}")]
+        //[MinLength(10, ErrorMessage = "{0}的长度至少{1}位")]
         public string Introduction { get; set; }
 
         //这种写法可以避免空引用异常
