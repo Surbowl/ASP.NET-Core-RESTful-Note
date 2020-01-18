79d39f8de78b79be7b533c71ffa128f89d438212
碗碗 <43742453+Surbowl@users.noreply.github.com>
2020-01-18 20:59:45 +0800

    Update to p27

--- Routine/Routine.APi/Models/EmployeeAddDto.cs
+++ Routine/Routine.APi/Models/EmployeeAddDto.cs
@@ -1,18 +1,43 @@
 ﻿using Routine.APi.Entities;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Routine.APi.Models
 {
-    public class EmployeeAddDto
+    public class EmployeeAddDto : IValidatableObject
     {
+        [Display(Name = "员工号")]
+        [Required(ErrorMessage = "{0}是必填项")]
+        [StringLength(10, MinimumLength = 10, ErrorMessage = "{0}的长度是{1}")]
         public string EmployeeNo { get; set; }
+
+        [Display(Name = "名")]
+        [Required(ErrorMessage = "{0}是必填项")]
+        [MaxLength(50, ErrorMessage = "{0}的长度不能超过{1}")]
         public string FirstName { get; set; }
+
+        [Display(Name = "姓"), Required(ErrorMessage = "{0}是必填项"), MaxLength(50, ErrorMessage = "{0}的长度不能超过{1}")]
         public string LastName { get; set; }
+
+        [Display(Name = "性别")]
         public Gender Gender { get; set; }
 
+        [Display(Name = "出生日期")]
         public DateTime DateOfBirth { get; set; }
+
+        /// <summary>
+        /// 自定义验证规则
+        /// </summary>
+        /// <param name="validationContext"></param>
+        /// <returns></returns>
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (FirstName == LastName)
+            {
+                //错误信息与引起错误的位置
+                yield return new ValidationResult("姓和名不能一样", new[] { nameof(LastName), nameof(FirstName) });
+            }
+        }
     }
 }
